b7787012291759d5428672c2e2d7ad0b00efc9a7
Anwar Andargie <anwarandargie3360@gmail.com>
2024-10-15 23:44:13 +0300

    add changes to the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "@/components/navbar/NavBar";
+import Provider from "./provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          <NavBar />
+          <main className="container py-10">{children}</main>
+        </Provider>
       </body>
     </html>
   );
