f78d58f6ed80e08319cf4faa010dca163502fc92
Anwar Andargie <anwarandargie3360@gmail.com>
2024-10-22 23:14:56 +0300

    add search bar functionalities to the home page

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,10 +1,40 @@
+"use client";
 import { Input } from "../ui/input";
+import { useState, useEffect } from "react";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 function NavSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleInputChange = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 1000);
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
   return (
     <Input
       type="text"
       placeholder="find a home..."
       className="max-w-xs dark:bg-muted"
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleInputChange(e.target.value);
+      }}
     />
   );
 }
